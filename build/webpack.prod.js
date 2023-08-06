3b21c0c4513212cddb0fe37410b894f64a9195a6
songzhongkun <13552186047@163.com>
2023-08-06 19:59:03 +0800

    生产环境下拷贝静态文件到构建出口文件中

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -1,8 +1,28 @@
+const path = require('path');
 const { merge } = require('webpack-merge');
+const CopyPlugin = require('copy-webpack-plugin');
+
 const baseConfig = require('./webpack.base.js');
 
 module.exports = merge(baseConfig, {
   mode: 'production',
 
   devtool: 'source-map',
-})
+
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, '../public'),
+          to: path.resolve(__dirname, '../dist'),
+          // filter: source => {
+          //   return !source.includes('index.html');
+          // },
+          globOptions: { 
+            ignore: ['**/index.html'], // 忽略html文件不进行复制
+          },
+        },
+      ],
+    }),
+  ],
+});
