9bf4248fb48c67852c67c6c2c4e5655cf63cd34a
songzhongkun <13552186047@163.com>
2023-08-06 19:29:16 +0800

    添加babel预设处理兼容

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -32,7 +32,14 @@ module.exports = {
           options: {
             presets: [
               [
-                "@babel/preset-typescript",
+                '@babel/preset-env',
+                {
+                  'useBuiltIns': 'usage', // 根据配置的浏览器兼容，以及代码中使用到的api进行引入polyfill按需添加
+                  'corejs': 3, // 配置使用core-js低版本
+                },
+              ],
+              [
+                '@babel/preset-typescript',
                 {
                   allExtensions: true,
                 }
