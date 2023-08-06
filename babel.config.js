9748e63e9f7ad0d6b7bb4546cbaa9cdccb050013
songzhongkun <13552186047@163.com>
2023-08-06 19:44:16 +0800

    解决开发环境corejs报错问题

--- babel.config.js
+++ babel.config.js
@@ -4,7 +4,7 @@ module.exports = {
       '@babel/preset-env',
       {
         'useBuiltIns': 'usage', // 根据配置的浏览器兼容，以及代码中使用到的api进行引入polyfill按需添加
-        'corejs': 3, // 配置使用core-js低版本
+        'corejs': '2.6.5', // 配置使用core-js低版本
       },
     ],
     [
